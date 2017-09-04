ea4bb0bc236391026912b8f94af9a26cab6a48a7
seiyaa <seiyamiro@163.com>
2017-09-04 17:47:24 +0800

    chapter 12

---
+++ write_message.py
@@ -0,0 +1,4 @@
+filename = 'programming.txt'
+with open(filename, 'w') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
