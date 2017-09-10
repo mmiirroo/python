bceb0f844c6cfa8b9a59085b984ba82230abccb9
seiyaaa <seiyamiro@163.com>
2017-09-10 23:20:05 +0800

    19.1.2 done

---
+++ learning_log/learning_logs/views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from .models import Topic
+from .forms import TopicForm
+
+# Create your views here.
+def index(request):
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
---
+++ python_repos.py
@@ -0,0 +1,25 @@
+import requests
+
+url='https://api.github.com/search/repositories?q=language:python&sort=stars'
+r =  requests.get(url)
+
+print("Status code:", r.status_code)
+response_dict = r.json()
+print("Total repositories:", response_dict['total_count'])
+
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+repo_dict = repo_dicts[0]
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
+
+print("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
