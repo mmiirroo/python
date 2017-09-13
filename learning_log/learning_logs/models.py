5d7abdf7c64ae7ae59d8d49242d8adc7c14cd4e1
seiyaa <seiyamiro@163.com>
2017-09-13 19:36:41 +0800

    P409

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.text
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,8 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,17 +13,22 @@ from .forms import TopicForm, EntryForm
 def index(request):
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -34,7 +40,8 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
-    
+
+@login_required    
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id = topic_id)
     if request.method != 'POST':
@@ -51,6 +58,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.object.get(id = entry_id)
     topic = entry.topic
