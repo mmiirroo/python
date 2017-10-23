1cd437309e7fea76d789bef9676921758883806d
seiyaa <seiyamiro@163.com>
2017-10-23 17:05:14 +0800

    iterate success

--- machine_learning/iris_plot.py
+++ machine_learning/iris_plot.py
@@ -1,6 +1,14 @@
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
+from Perceptron import perceptron
+
+matplotlib.matplotlib_fname()
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+df.tail()
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
@@ -9,4 +17,11 @@ plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versico
 plt.xlabel('sepal length')
 plt.ylabel('petal length')
 plt.legend(loc='upper left')
+#plt.show()
+
+ppn = perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
 plt.show()
